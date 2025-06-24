62df3e66d84c58c928d071c87fa7d1474b142dd0
Alexnis-Herrera <alexnisherrerag@gmail.com>
2025-06-24 14:48:08 +0000

    JuegoAlexnis

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
-//write your code here
-console.log("Hello Rigo from the console!");
+const Game = ["rock", "paper", "scissors", "lizard", "spock"]
+
+const opcionesJuego = () => {
+    const index = Math.floor(Math.random() * Game.length)
+    return Game[index]
+}
+
+const comprobarOpciones = (Usuario) => {
+    const jugador1 = opcionesJuego ()
+
+if (Usuario === jugador1) {
+    console.log("Haz ganado");
+    
+} else {
+    console.log("sigue intentando");
+    
+}
+
+const guess = prompt ("Selecciona alguna de las opciones: rock, paper, scissors, lizard, spock").toLowerCase()
+checkGameGuess(guess)
+
+
+}
+
+
+
+ 
 
