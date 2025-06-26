99008b164a95a801cd7ee99fbacde9e8af9f195b
Alexnis-Herrera <alexnisherrerag@gmail.com>
2025-06-26 10:13:52 +0000

    ProyectoPiedraPapelTijera

--- src/app.js
+++ src/app.js
@@ -1,28 +1,29 @@
-const Game = ["rock", "paper", "scissors", "lizard", "spock"]
-
-const opcionesJuego = () => {
-    const index = Math.floor(Math.random() * Game.length)
-    return Game[index]
+// CREAR OPCION 
+const opciones = ["rock", "paper", "scissors", "lizard", "spock"];
+// REGLAS DEL JUEGO
+const reglas = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["paper", "spock"],
+    spock: ["scissors", "rock"],
+};
+//GENERAR OPCION ALEATORIA
+const maquina = () => {
+    return opciones[Math.floor(Math.random() * opciones.length)]
+};
+//COMPARAR OPCIONES PARA REGLAS DE JUEGO
+const game = (usuario) => {
+    const opcionMaquina = maquina();
+    if (usuario == opcionMaquina){
+        return "Empate";
+    }
+    else if (reglas[usuario].includes(opcionMaquina)){
+        return "Ganaste";
+    }
+    else {
+        return "Perdiste"
+    }        
 }
-
-const comprobarOpciones = (Usuario) => {
-    const jugador1 = opcionesJuego ()
-
-if (Usuario === jugador1) {
-    console.log("Haz ganado");
-    
-} else {
-    console.log("sigue intentando");
-    
-}
-
-const guess = prompt ("Selecciona alguna de las opciones: rock, paper, scissors, lizard, spock").toLowerCase()
-checkGameGuess(guess)
-
-
-}
-
-
-
- 
-
+//MOSTRAR EL RESULTADO
+console.log(game("rock"))
